e87b493e49b3a2fb954b737643cf0b7bd6c91443
Flávio <flaviojunior0899@gmail.com>
2020-06-13 13:42:02 -0300

    creating gui

--- showcase/src/app/app.component.ts
+++ showcase/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import Car from './models/car.model';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CarMockerService } from './services/car-mocker.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'showcase';
+  
+  apiResponse = 'Make some request!';
+  logger = '...';
+  form: FormGroup;
+
+  car = new Car();
+
+  constructor(
+    private carService: CarMockerService
+  ) { }
+
+  onCreate(): void {
+    this.carService.create(this.car).subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+      this.logger = 'New car saved!';
+      this.car = new Car();
+    });
+  }
+
+  onReadAll(): void {
+    this.carService.index().subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+      this.logger = 'All cars listed!';
+    })
+  }
+
+  onReadOne(): void {
+    this.carService.show(this.car.id).subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+      this.logger = 'Car showed!';
+    });
+  }
+
+  onUpdate(): void {
+    this.carService.update(this.car).subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+      this.logger = 'Car updated!';
+      this.car = new Car();
+    });
+  }
+
+  onDelete(): void {
+    this.carService.destroy(this.car.id).subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+      this.logger = 'Car deleted!';
+    })
+  }
+
 }
